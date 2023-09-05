cb53e57ec18afc6a4d79ba6cf5980ffe3b428979
AnsaKhalil <118052478+AnsaKhalil@users.noreply.github.com>
2023-09-05 17:38:38 +0500

    Updated TodoList.tsx

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -16,6 +16,7 @@ export const AddTask = () => {
     const [list, setList] = useState<list[]>([]);
 
     const addTask = ()=>{
+        if (!task || /^\s*$/.test(task)){return;}
         const newTask = {
             id: list.length === 0 ? 1 : list[list.length - 1].id + 1,
             taskName: task,
@@ -23,6 +24,7 @@ export const AddTask = () => {
             isEditting: false
         }
         setList([...list, newTask]);
+        setTask("");
     }
 
     const deleteTask = (id: number) => {
@@ -32,20 +34,17 @@ export const AddTask = () => {
         setList([]);
     }
     const isEditTask = (id: number) => {
-        setList(list.map((task)=> task.id === id ?
-         {...task, isEditting: !task.isEditting} : task
-        ))
-    }
-    const cancelEdit = (id:number) => {
-        setList(list.map((task)=> task.id === id ?
-         {...task, isEditting: !task.isEditting} : task
+        // @ts-ignore
+        setList(list.map((task)=> task.id === id ? {...task, taskName:setTaskValue(task.taskName), isEditting: !task.isEditting} : task
         ))
     }
 
     const editTask = (id:number) => {
+        if (!taskValue || /^\s*$/.test(taskValue)){return;}
         setList(list.map((task)=> task.id === id ?
          {...task, taskName: taskValue, isEditting: !task.isEditting} : task
         ))
+        
     }
 
     const updateTask = (id: number) => {
@@ -60,17 +59,17 @@ export const AddTask = () => {
     }
 
     return <div className="todoApp">
-        <h1>Make Your Own "TO DO LIST" Now! ˚ʚ♡ɞ˚</h1>
+        <h1>Make Your Own "TO DO LIST" ˚ʚ♡ɞ˚</h1>
         <div className="addTask">
-    <input type="text" placeholder="Add Task..." onChange={(e)=>{ setTask(e.target.value)}}/>
+    <input type="text" placeholder="Add Task..." value={task} onChange={(e)=>{ setTask(e.target.value)}}/>
     <button onClick={addTask}>Add</button>
     <button onClick={deleteAllTasks}>Delete All</button>
     
     </div>
     <div className="taskList">
     {list?.map((task) => (
-        task.isEditting ? (<div><input type="text" placeholder={task.taskName} onChange={(e)=>{ setTaskValue(e.target.value)}}/>
-    <button onClick={()=>editTask(task.id)}>Edit</button><button onClick={()=>cancelEdit(task.id)}>Cancel</button></div>) : (<div>
+        task.isEditting ? (<div><input type="text" placeholder={task.taskName} value={taskValue} onChange={(e)=>{ setTaskValue(e.target.value)}}/>
+    <button onClick={()=>editTask(task.id)}>Update</button></div>) : (<div>
         
             <h5 key={task.id} style={{color: task.completed ? "green" : "white"}}>{task.taskName}</h5>
             {task && (<div><button onClick={()=>deleteTask(task.id)}><BsTrash3/></button>
